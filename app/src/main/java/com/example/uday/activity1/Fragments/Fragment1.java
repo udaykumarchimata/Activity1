8e76ddfbf5a8e14c60373210bcbd54c49f042ab9
udaykumarchimata <udaykumarchimata@gmail.com>
2016-09-14 02:22:53 -0500

    update1

--- app/src/main/java/com/example/uday/activity1/Fragments/Fragment1.java
+++ app/src/main/java/com/example/uday/activity1/Fragments/Fragment1.java
@@ -17,7 +17,7 @@ public class Fragment1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_layout,container,false);
+        
 
     }
 }
